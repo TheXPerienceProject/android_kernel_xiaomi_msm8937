3ff701d79b0cd89edd9c28e7e284e68cdf88ff04
Daniel Rosenberg <drosen@google.com>
2018-01-18 16:17:16 -0800

    ANDROID: sdcardfs: Move default_normal to superblock
    Moving default_normal from mount info to superblock info
    as it doesn't need to change between mount points.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Bug: 72158116
    Change-Id: I16c6a0577c601b4f7566269f7e189fcf697afd4e
    Signed-off-by: klozz <klozz707@gmail.com>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -68,6 +68,7 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	opts->reserved_mb = 0;
 	/* by default, gid derivation is off */
 	opts->gid_derivation = false;
+	opts->default_normal = false;
 
 	*debug = 0;
 
@@ -121,6 +122,9 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 			break;
 		case Opt_gid_derivation:
 			opts->gid_derivation = true;
+			break;
+		case Opt_default_normal:
+			opts->default_normal = true;
 			break;
 		/* unknown option */
 		default:
