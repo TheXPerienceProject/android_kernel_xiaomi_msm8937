eb105a84355bbeb4b00eb22fb4eb517496a451e9
klozz <klozz707@gmail.com>
2018-02-05 05:42:48 -0500

    sdcardfs: fix miss declaration of Opt_default_normal
    Signed-off-by: klozz <klozz707@gmail.com>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -33,6 +33,7 @@ enum {
 	Opt_userid,
 	Opt_reserved_mb,
 	Opt_gid_derivation,
+	Opt_default_normal,
 	Opt_err,
 };
 
@@ -45,6 +46,7 @@ static const match_table_t sdcardfs_tokens = {
 	{Opt_userid, "userid=%d"},
 	{Opt_multiuser, "multiuser"},
 	{Opt_gid_derivation, "derive_gid"},
+	{Opt_default_normal, "default_normal"},
 	{Opt_reserved_mb, "reserved_mb=%u"},
 	{Opt_err, NULL}
 };
@@ -59,6 +61,7 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 
 	/* by default, we use AID_MEDIA_RW as uid, gid */
 	opts->fs_low_uid = AID_MEDIA_RW;
+	opts->fs_low_gid = AID_MEDIA_RW;
 	vfsopts->mask = 0;
 	opts->multiuser = false;
 	opts->fs_user_id = 0;
@@ -178,6 +181,7 @@ int parse_options_remount(struct super_block *sb, char *options, int silent,
 				return 0;
 			vfsopts->mask = option;
 			break;
+		case Opt_default_normal:
 		case Opt_multiuser:
 		case Opt_userid:
 		case Opt_fsuid:
